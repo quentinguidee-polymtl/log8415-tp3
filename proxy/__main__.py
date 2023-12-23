b9744536610667530c7925ff7aaa3f0228425293
Quentin Guidée <git@arra.red>
2023-12-23 11:09:04 -0500

    Create a basic skeleton for the Proxy

---
+++ proxy/__main__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/direct")
+def handle_direct():
+    """
+    Directly send the request to the master node
+    """
+    pass
+
+
+@app.route("/random")
+def handle_random():
+    """
+    Send the request to a random slave node
+    """
+    pass
+
+
+@app.route("/custom")
+def handle_custom():
+    """
+    Send the request to the slave with the least load
+    """
+    pass
+
+
+if __name__ == "__main__":
+    app.run()
