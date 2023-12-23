80b5d1aa7ca63cce36c8e01870c6fb89a5b511b1
Quentin Guidée <git@arra.red>
2023-12-23 11:29:49 -0500

    gatekeeper: Create a basic gatekeeper handler

--- gatekeeper/__main__.py
+++ gatekeeper/__main__.py
@@ -1,14 +1,29 @@
-from flask import Flask
+import os
+
+import requests
+from flask import Flask, request
 
 app = Flask(__name__)
 
+PROXY_HOST = os.environ.get("PROXY_HOST")
 
-@app.route("/<method>")
+
+@app.route("/<method>", methods=["GET", "POST"])
 def handle(method: str):
     """
     Handle /direct, /random, and /custom requests
     """
-    pass
+    url = f"http://{PROXY_HOST}/{method}"
+
+    match request.method:
+        case "GET":
+            res = requests.get(url)
+        case "POST":
+            res = requests.post(url)
+        case _:
+            return "Method not allowed", 405
+
+    return res.content
 
 
 if __name__ == "__main__":
